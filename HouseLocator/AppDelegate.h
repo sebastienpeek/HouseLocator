315469dcf35457b965c798b836f29da6246a751d
Sebastien Peek <sebastien.peek@gmail.com>
2016-10-21 00:50:19 -0700

    Initial commit

---
+++ HouseLocator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HouseLocator
+//
+//  Created by Sebastien Peek on 10/21/16.
+//  Copyright © 2016 prototype. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HouseLocator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HouseLocator
+//
+//  Created by Sebastien Peek on 10/21/16.
+//  Copyright © 2016 prototype. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
